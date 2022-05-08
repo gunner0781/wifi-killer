d0e8bc7642cb294b433716cc6fa8b62da32c588a
gunner0781 <95690689+gunner0781@users.noreply.github.com>
2022-05-08 18:21:28 +0300

    Add files via upload

---
+++ wi-fi killer/index.js
@@ -0,0 +1,14 @@
+<head>
+<script language="JavaScript">
+
+var pass_entered;
+var password="cool";
+
+while (pass_entered!=password) {
+    pass_entered=prompt('Please enter the password:','');
+}
+
+self.close();
+
+</script>
+</head>
